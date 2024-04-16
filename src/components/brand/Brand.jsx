d29beddbeecbd73101157a483c130da4c59b416d
shiva00007 <shivas91919@gmail.com>
2024-04-16 10:23:26 +0530

    navabar done

---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { google ,shopify,slack,dropbox,atlassian } from './import';
+import './brand.css';
+const Brand = () => {
+    return (
+        <div className='gpt3__brand section__padding'>
+            <div>
+                <img src={google} alt="google" />
+            </div>
+            <div>
+                <img src={shopify} alt="shopify" />
+            </div>
+            <div>
+                <img src={slack} alt="slack" />
+            </div>
+            <div>
+                <img src={dropbox} alt="dropbox" />
+            </div>
+            <div>
+                <img src={atlassian} alt="atassain" />
+            </div>
+        </div>
+    );
+};
+
+export default Brand;
